6b68a4a6edcb1d84598394a135c7fc685b2d2b29
Vin <winlqvin.it@gmail.com>
2022-06-23 19:31:24 +0700

    updated

--- node_env_setup.sh
+++ node_env_setup.sh
@@ -5,7 +5,12 @@ sudo apt-get install -y build-essential curl git cups vim
 
 # install nvm
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+sleep 1
+
 source ~/.bashrc
+
+sleep 1
 
 # install node + yarn
 nvm install v14.16.0
