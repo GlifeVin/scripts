4edf9b81a62473a564f89e721996648c0f0d0a65
Vin <winlqvin.it@gmail.com>
2022-06-23 20:01:24 +0700

    updated

--- node_env_setup.sh
+++ node_env_setup.sh
@@ -13,5 +13,9 @@ source $NVM_DIR/nvm.sh
 # install nodejs + yarn
 nvm install v14.16.0
 nvm use v14.16.0
+
+# remove symlink before install new one
+rm -rf $NVM_DIR/versions/node/v14.16.0/bin/corepack
 npm i -g corepack
+
 npm i -g pm2
