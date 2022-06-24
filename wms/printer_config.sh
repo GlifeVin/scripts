406670a6bd21e4971ea0d366389e43685b38b4a2
Vin <winlqvin.it@gmail.com>
2022-06-24 18:06:11 +0700

    updated

--- wms/printer_config.sh
+++ wms/printer_config.sh
@@ -3,19 +3,19 @@
 PRINTER_NAME="GP-3150TN"
 PRINTER_URI=`lpstat -v | grep $PRINTER_NAME | sed 's/^.*usb:/usb:/g'`
 
-if [ ! -e ~/tmp/wms/Gprinter.ppd ]
+if [ ! -e ~/wms/Gprinter.ppd ]
 then
-  sudo mkdir -p ~/tmp/wms
-  sudo chown `whoami`:`whoami` ~/tmp/wms
-  curl https://raw.githubusercontent.com/GlifeVin/scripts/main/wms/Gprinter.ppd > ~/tmp/wms/Gprinter.ppd
+  sudo mkdir -p ~/wms
+  sudo chown `whoami`:`whoami` ~/wms
+  curl https://raw.githubusercontent.com/GlifeVin/scripts/main/wms/Gprinter.ppd > ~/wms/Gprinter.ppd
 fi
 
-if [ ! -e ~/tmp/wms/rastertotspl ]
+if [ ! -e ~/wms/rastertotspl ]
 then
-  sudo mkdir -p ~/tmp/wms
-  sudo chown `whoami`:`whoami` ~/tmp/wms
-  curl https://raw.githubusercontent.com/GlifeVin/scripts/main/wms/rastertotspl > ~/tmp/wms/rastertotspl
-  sudo cp ~/tmp/wms/rastertotspl /usr/lib/cups/filter/rastertotspl
+  sudo mkdir -p ~/wms
+  sudo chown `whoami`:`whoami` ~/wms
+  curl https://raw.githubusercontent.com/GlifeVin/scripts/main/wms/rastertotspl > ~/wms/rastertotspl
+  sudo cp ~/wms/rastertotspl /usr/lib/cups/filter/rastertotspl
   sudo chown root:root /usr/lib/cups/filter/rastertotspl
   sudo chmod 755 /usr/lib/cups/filter/rastertotspl
 fi
@@ -23,9 +23,9 @@ fi
 echo "Printer $PRINTER_NAME"
 echo "Printer uri $PRINTER_URI"
 
-sudo /usr/sbin/lpadmin -p $PRINTER_NAME -E -v $PRINTER_URI -P ~/tmp/wms/Gprinter.ppd -o usb-no-reattach=true -o usb-unidir=true
+sudo /usr/sbin/lpadmin -p $PRINTER_NAME -E -v $PRINTER_URI -P ~/wms/Gprinter.ppd -o usb-no-reattach=true -o usb-unidir=true
 
 sudo service cups restart
 
 # cleanup
-sudo rm -rf ~/tmp/wms
+sudo rm -rf ~/wms
