0a6af64ab409a623e7f1bc00c6156c96364f93e3
Vin <winlqvin.it@gmail.com>
2022-06-24 10:11:04 +0700

    updated

--- wms/printer_config.sh
+++ wms/printer_config.sh
@@ -2,17 +2,24 @@
 
 PRINTER_NAME="GP-3150TN"
 PRINTER_URI=`lpstat -v | grep $PRINTER_NAME | sed 's/^.*usb:/usb:/g'`
-# if [! -e ~/tmp/wms/Gprinter.ppd ]
-# then
-#   sudo mkdir -p ~/tmp/wms
-#   curl https://raw.githubusercontent.com/GlifeVin/scripts/main/wms/Gprinter.ppd > ~/tmp/wms/Gprinter.ppd
-# fi
 
+if [ ! -e ~/tmp/wms/Gprinter.ppd ]
+then
+  sudo mkdir -p ~/tmp/wms
+  curl https://raw.githubusercontent.com/GlifeVin/scripts/main/wms/Gprinter.ppd > ~/tmp/wms/Gprinter.ppd
+fi
+
+if [ ! -e ~/tmp/wms/rastertotspl ]
+then
+  sudo mkdir -p ~/tmp/wms
+  curl https://raw.githubusercontent.com/GlifeVin/scripts/main/wms/rastertotspl > ~/tmp/wms/rastertotspl
+  sudo mv ~/tmp/wms/rastertotspl /usr/lib/cups/filter/rastertotspl
+fi
 
 echo "Printer $PRINTER_NAME"
 echo "Printer uri $PRINTER_URI"
 
-sudo /usr/sbin/lpadmin -p $PRINTER_NAME -E -v $PRINTER_URI -m https://raw.githubusercontent.com/GlifeVin/scripts/main/wms/Gprinter.ppd
+sudo /usr/sbin/lpadmin -p $PRINTER_NAME -E -v $PRINTER_URI -P ~/tmp/wms/Gprinter.ppd
 
 # config
 sudo lpadmin -p $PRINTER_NAME -o usb-no-reattach=true
