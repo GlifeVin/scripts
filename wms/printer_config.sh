5f703fc4ff19ec5cef60acbb5969f70e03802de5
Vin <winlqvin.it@gmail.com>
2022-06-24 10:30:52 +0700

    updated

--- wms/printer_config.sh
+++ wms/printer_config.sh
@@ -6,16 +6,18 @@ PRINTER_URI=`lpstat -v | grep $PRINTER_NAME | sed 's/^.*usb:/usb:/g'`
 if [ ! -e ~/tmp/wms/Gprinter.ppd ]
 then
   sudo mkdir -p ~/tmp/wms
-  sudo chown `whoami` ~/tmp/wms
+  sudo chown `whoami`:`whoami` ~/tmp/wms
   curl https://raw.githubusercontent.com/GlifeVin/scripts/main/wms/Gprinter.ppd > ~/tmp/wms/Gprinter.ppd
 fi
 
 if [ ! -e ~/tmp/wms/rastertotspl ]
 then
   sudo mkdir -p ~/tmp/wms
-  sudo chown `whoami` ~/tmp/wms
+  sudo chown `whoami`:`whoami` ~/tmp/wms
   curl https://raw.githubusercontent.com/GlifeVin/scripts/main/wms/rastertotspl > ~/tmp/wms/rastertotspl
-  sudo mv ~/tmp/wms/rastertotspl /usr/lib/cups/filter/rastertotspl
+  sudo cp ~/tmp/wms/rastertotspl /usr/lib/cups/filter/rastertotspl
+  sudo chown root:root /usr/lib/cups/filter/rastertotspl
+  sudo chmod 755 /usr/lib/cups/filter/rastertotspl
 fi
 
 echo "Printer $PRINTER_NAME"
